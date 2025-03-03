c2e3362937af65f54cbd0bea9ef9eec1b789ca34
Manish <patelmanish131149198@gmail.com>
2025-03-04 00:03:04 +0530

    [Manish] add. introducing logging in properties

--- SpringAddressBookApplication/src/main/java/com/bridgelabz/springaddressbookapplication/SpringAddressBookApplication.java
+++ SpringAddressBookApplication/src/main/java/com/bridgelabz/springaddressbookapplication/SpringAddressBookApplication.java
@@ -1,13 +1,18 @@
 package com.bridgelabz.springaddressbookapplication;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class SpringAddressBookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAddressBookApplication.class, args);
+		log.info("Address Book App Started");
 	}
 
 }
