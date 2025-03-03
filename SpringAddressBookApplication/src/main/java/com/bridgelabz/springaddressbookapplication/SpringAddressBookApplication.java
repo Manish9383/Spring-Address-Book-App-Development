8b009571045459858ca64d19391402a4b1b8c58b
Manish <patelmanish131149198@gmail.com>
2025-03-04 00:12:58 +0530

    [Manish] add. database setting as environment variable

--- SpringAddressBookApplication/src/main/java/com/bridgelabz/springaddressbookapplication/SpringAddressBookApplication.java
+++ SpringAddressBookApplication/src/main/java/com/bridgelabz/springaddressbookapplication/SpringAddressBookApplication.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.springaddressbookapplication;
 
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,6 +15,7 @@ public class SpringAddressBookApplication {
 	public static void main(String[] args) {
 		ApplicationContext context=SpringApplication.run(SpringAddressBookApplication.class, args);
 		log.info("Address Book App Started in {} Environment",context.getEnvironment().getProperty("environment"));
+		log.info("Address Book DB User is {} ",context.getEnvironment().getProperty("spring.datasource.username"));
 	}
 
 }
